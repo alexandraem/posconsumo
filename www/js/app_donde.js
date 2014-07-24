d145bec623e797a7989d6c5fa68a5e6eb37aa523
alexandraem <alexandra.e.mejia@gmail.com>
2014-07-24 00:13:11 -0500

    sin logo en acerca de

--- www/js/app_donde.js
+++ www/js/app_donde.js
@@ -41,7 +41,8 @@ ambienteApp.controller( 'LugaresCategoriasCtrl', ['$scope', '$http', '$location'
         $scope.categoria = $routeParams.categoria
 
         $scope.state = "Cargando departamentos..."
-
+        //...................................toolbar.hide().............//
+        toolbar.hide()
         mostrarCargando("Cargando departamentos")
         getDeptos(function (departamentos) {
             $scope.departs = departamentos
@@ -62,6 +63,8 @@ ambienteApp.controller( 'LugaresCategoriasCtrl', ['$scope', '$http', '$location'
         $scope.cargarMunicipios = function () {
 
             $scope.state = "Cargando municipios..."
+            //...................................toolbar.hide().............//
+            toolbar.hide()
             var CodDpto = $scope.deptoSel;
             mostrarCargando("Cargando municipios");
             $.getJSON(
